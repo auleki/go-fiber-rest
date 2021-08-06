69e058231ffb77118b340576719238f49bab8fd8
auleki <kodagiwa@gmail.com>
2021-08-06 21:07:52 +0100

    added get single todo comp...

--- controllers/todo.go
+++ controllers/todo.go
@@ -107,3 +107,67 @@ func GetTodo(c *fiber.Ctx) error {
 		"message": "Todo not found"
 	})
 }
+
+func UpdateTodo(c *fiber.Ctx) error {
+	type Request struct {
+		Title *string `json:"title"`
+		Completed *bool `json:"completed"`
+	}
+
+	
+	
+	// extract parameter id
+	paramId := c.Params("id")
+
+	// convert id from string to int
+	id, err := strconv.Atoi(paramId)
+
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"success": false,
+			"message": "Cannot parse ID"
+		})
+	}
+
+	var body Request
+
+	err = c.BodyParser(&body)
+
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"success": false,
+			"message": "Cannot parse JSON"
+		})
+	}
+
+	var todo *Todo 
+
+	for _, t = range todos {
+		if t.Id == id {
+			todo = t
+			break
+		}
+	}
+
+	if todo.Id == 0 {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"success": false,
+			"message": "Not Found"
+		})
+	}
+
+	if body.Title != nil {
+		todo.Title = *body.Title
+	}
+
+	if body.Completed != nil {
+		todo.Completed = *body.Completed
+	}
+	
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"success": true,
+		"data": fiber.Map{
+			"todo": todo,
+		}
+	})
+}
