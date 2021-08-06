1342373e26b3379a0136e34340853ba737462ba9
auleki <kodagiwa@gmail.com>
2021-08-06 20:24:42 +0100

    modularizing route matching functions

--- controllers/todo.go
+++ controllers/todo.go
@@ -1,7 +1,38 @@
 package controllers
 
+import "github.com/gofiber/fiber/v2"
+
 type Todo struct {
 	Id        int    `json:"id"`
 	Title     string `json:"title"`
 	Completed bool   `json:"completed"`
+}
+
+// static typed todos
+var todos = []*Todo{
+	{
+		Id:        1,
+		Title:     "Record a verse",
+		Completed: false,
+	},
+	{
+		Id:        3,
+		Title:     "Fucking breath",
+		Completed: true,
+	},
+	{
+		Id:        2,
+		Title:     "Complete data entry profile",
+		Completed: false,
+	},
+}
+
+// gets all todos
+func GetTodos(c *fiber.Ctx) error {
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"success": true,
+		"data": fiber.Map{
+			"todos": todos,
+		},
+	})
 }
--- main.go
+++ main.go
@@ -5,16 +5,35 @@ import (
 	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
-func main() {
-	app := fiber.New()
-	app.Use(logger.New())
+func setupRoutes(app *fiber.App) {
+	//route to get all todos
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.Status(fiber.StatusOK).JSON(fiber.Map{
 			"success": true,
 			"message": "Home endpoint",
 		})
 	})
+
+	api := app.Group("/api")
+
+	api.Get("", func(c *fiber.Ctx) error {
+		return c.Status(fiber.StatusOK).JSON(fiber.Map{
+			"success": true,
+			"message": "AT THE NEW ENDPOINT",
+		})
+	})
+}
+
+func main() {
+	app := fiber.New()
+	app.Use(logger.New())
+	// setup Routes
+	setupRoutes(app)
+
+	// listen on port 8000 for any network requests
 	err := app.Listen(":8000")
+
+	// handle error
 	if err != nil {
 		panic(err)
 	}
